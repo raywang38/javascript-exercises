f2262d6724da2ccb25007fb8d1b5b90aed08a514
Ray <170437057+raywang38@users.noreply.github.com>
2024-06-21 02:30:10 +1000

    complete 01 and 02 exercises for javascript
    Completion of first two javascript exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let stringOutput = "";
 
+    for (let i = 0; i < num; i++) {
+        stringOutput += string;
+    }
+
+    return stringOutput;
 };
 
 // Do not edit below this line
