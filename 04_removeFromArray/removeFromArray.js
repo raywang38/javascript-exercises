00bda5b98448eefb4de843e935a32c6a36593cdd
Ray <170437057+raywang38@users.noreply.github.com>
2024-06-21 17:38:45 +1000

    finish 04 js exercise
    Finish the 04 js exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(origArray, ...args) {
+
+    const newArray = [...new Set(origArray)];
+
+    for (let eachArg of args) {
+        if (newArray.includes(eachArg)) {
+            newArray.splice(newArray.indexOf(eachArg), 1);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
