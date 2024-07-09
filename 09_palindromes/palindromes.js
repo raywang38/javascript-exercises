89d0df34fbc3d9d8e0edb8397bca42d08f37ce52
Ray <170437057+raywang38@users.noreply.github.com>
2024-07-09 18:51:26 +1000

    complete 09 JS exercise
    Completion of 09 JS exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const stringArray = string.replace(/[\.,?! ]/g, "").toLowerCase().split("");
+    const reverseStringArray = string.replace(/[\.,?! ]/g, "").toLowerCase().split("").reverse();
 
+    for (i in stringArray) {
+        if (stringArray[i] === reverseStringArray[i]) {
+            continue;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
