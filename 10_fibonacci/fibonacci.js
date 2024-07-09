0ff159d64af3256436b84b8e38a55088274571bc
Ray <170437057+raywang38@users.noreply.github.com>
2024-07-10 00:04:50 +1000

    complete 10 JS exercise
    Completion of 10 JS exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let sum = 0;
+    let val1 = 1;
+    let val2 = 1;
 
+    if (!isNaN(num)) {
+        num = parseInt(num);
+    }
+
+    if (num < 0) {
+        return "OOPS";
+    }
+    else if (num === 0) {
+        return sum;
+    }
+    else if (num === 1) {
+        return val1;
+    }
+    else if (num === 2) {
+        return val2;
+    }
+
+    // 1, 1, 2, 3, 5, 8, 13
+    for (let i = 0; i < num - 2; i++) {
+        sum = val1 + val2;
+        val1 = val2;
+        val2 = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
