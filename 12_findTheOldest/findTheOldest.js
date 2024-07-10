f5a110423e4178eefb42ebb13aea8b4215904318
Ray <170437057+raywang38@users.noreply.github.com>
2024-07-11 08:30:06 +1000

    finish 12 JS exercise
    Completion of 12 JS exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const sortedAgeArray = arr.reduce((personArray, person) => {
+        if (!("yearOfDeath" in person)) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        const obj = {
+            name : person.name,
+            age : person.yearOfDeath - person.yearOfBirth    
+        };
+        personArray.push(obj);
+        return personArray;
+    }, []).sort((a, b) => a.age > b.age ? 1 : -1);
+    const oldest = sortedAgeArray.pop();
+    delete oldest.age;
+    return oldest;
 };
 
 // Do not edit below this line
